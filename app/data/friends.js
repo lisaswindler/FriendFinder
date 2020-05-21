46238cfd3f532b662f189c49f60a5df0d397b2fe
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-05-21 00:23:17 -0600

    files set up, working on js logic

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var friendArray = [
+    {
+        name: 'Ivy',
+        photo: 'https://lisaswindler.github.io/images/lisapic-square.jpg',
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: 'Maren',
+        photo: 'https://lisaswindler.github.io/images/lisapic-square.jpg',
+        scores: [
+            4,
+            1,
+            3,
+            2,
+            1,
+            1,
+            3,
+            4,
+            3,
+            2
+        ]
+    },
+    {
+        name: 'Lisa',
+        photo: 'https://lisaswindler.github.io/images/lisapic-square.jpg',
+        scores: [
+          5, 
+          4, 
+          3, 
+          2,
+          4, 
+          1, 
+          3, 
+          1,
+          2, 
+          5
+        ]
+      }
+];
+
+module.exports = friendArray;
+
+
+
