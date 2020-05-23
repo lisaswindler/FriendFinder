5d59fac8ea67f13e7506a7b099f32041270c63b8
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-05-22 20:34:27 -0600

    js complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,24 @@
 var friendArray = [
     {
         name: 'Ivy',
-        photo: 'https://lisaswindler.github.io/images/lisapic-square.jpg',
-        scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
+        photo: 'https://1.bp.blogspot.com/-na8yrcimqRM/XshypE8hTzI/AAAAAAAAEBM/UaWKgngEiMoRNhZJGucSxV4BTaHCdg8dwCLcBGAsYHQ/s1600/Ivy_friend.jpeg',
+        scores: [4, 4, 1, 2, 1, 1, 3, 2, 3, 4]
     },
     {
         name: 'Maren',
-        photo: 'https://lisaswindler.github.io/images/lisapic-square.jpg',
-        scores: [
-            4,
-            1,
-            3,
-            2,
-            1,
-            1,
-            3,
-            4,
-            3,
-            2
-        ]
+        photo: 'https://1.bp.blogspot.com/-RZrxgYTV-ko/Xshypd-BwpI/AAAAAAAAEBQ/me88PuzMaLI487z1KwiaDQo554jCHf3ogCLcBGAsYHQ/s1600/Maren_friend.jpeg',
+        scores: [3, 2, 3, 2, 1, 2, 2, 2, 3, 1]
     },
     {
         name: 'Lisa',
         photo: 'https://lisaswindler.github.io/images/lisapic-square.jpg',
-        scores: [
-          5, 
-          4, 
-          3, 
-          2,
-          4, 
-          1, 
-          3, 
-          1,
-          2, 
-          5
-        ]
-      }
+        scores: [5, 4, 3, 2, 4, 1, 3, 1, 2, 4]
+    },
+    {
+        name: 'Lucy',
+        photo: 'https://cdn.pixabay.com/photo/2014/07/11/22/05/baby-390555_1280.jpg',
+        scores: [5, 1, 1, 1, 1, 5, 5, 1, 1, 1]
+    }
 ];
 
 module.exports = friendArray;
