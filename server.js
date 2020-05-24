5df3cb7fc3c90220e7d83f8b99ea4db9b58bc33e
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-05-23 18:00:11 -0600

    deploying to heroku

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(_dirname + '/public'));
+// app.use('/static', express.static(__dirname + '/public'));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
