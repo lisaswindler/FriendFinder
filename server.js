105524e8ed9a52e5a35921ec04a77d7fed276caf
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-05-24 01:40:20 -0600

    styling in progress

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(_dirname + '/public'));
-// app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static('app/public'));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
